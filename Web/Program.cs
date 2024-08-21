c6b5f9564ef857fb9f0062b446b7fc5155837247
Binh Duc Tran <binhtran432k@gmail.com>
2024-08-21 20:05:53 +0700

    feat: implement user service

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,11 +1,18 @@
+using Blazored.LocalStorage;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MiniAssetManagement;
+
+using Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
+
+Configuration.ConfigureCommonServices(builder.Services);
 
 await builder.Build().RunAsync();
